ee525541b27deecbf51884d79b957aeb9cd3edc1
Zolival <zolinak@gmail.com>
2021-11-01 18:34:25 +0100

    works, but only the console sees the winner

--- js-1-feladatok-tictactoe/js.js
+++ js-1-feladatok-tictactoe/js.js
@@ -2,46 +2,60 @@
 
 const horizontalCells = 5;
 const verticalCells = 5;
-let resultNumber = 3; //or 5
+let resultNumber = 3 ; //or 5
 let xPlayer = [];
 let yPlayer = [];
 
-const winnerTest = (id)=>{
+let playerIcon = 'X';
+
+const winnerTest = (id, playersign)=>{
     let indexOf = id.indexOf('-');
     let rowIndex = parseFloat(id.slice(0, indexOf));
     let colIndex = parseFloat(id.slice(indexOf+1 , ));
-    console.log('rowIndex = ', rowIndex);
-    console.log('colIndex = ', colIndex);
-    // let idnumber = [rowIndex,  colIndex]
-    let idNumber = `${colIndex}${colIndex}`
-    console.log('idnumberArray = ', typeof parseFloat(idNumber));
-
-    
-
-    let rowControlNegative = [`${colIndex}-${rowIndex -1}` ,`${colIndex}-${rowIndex -2}`];
-    let rowControlPositive = [`${colIndex}-${rowIndex +1}` , `${colIndex}-${rowIndex +2}`];
-    console.log('rowControlNegative = ', rowControlNegative);
-    console.log('rowControlPositive = ', rowControlPositive);
-    let colControlNegative = [`${colIndex -1}-${rowIndex}`, `${colIndex -2}-${rowIndex}`];
-    let colControlPositive = [`${colIndex +1}-${rowIndex}`, `${colIndex +2}-${rowIndex}`];
-
-    
-    let obliqueControlNegative1 = [`${colIndex -1}-${rowIndex -1}`, `${colIndex -2}-${rowIndex -2}`];
-    let obliqueControlNegative2 = [`${colIndex -1}-${rowIndex +1}` , `${colIndex -2}-${rowIndex +2}`];
-    
-    let obliqueControlPositive1 = [`${colIndex +1}-${rowIndex +1}`, `${colIndex +2}-${rowIndex +2}`];
-    let obliqueControlPositive2 = [`${colIndex +1}-${rowIndex -1}`, `${colIndex +2}-${rowIndex -2}`];
-
-    /*
-    let h = colIndex;
-    for( h ; h < colIndex + resultNumber ; h++ ){
-        console.log('forColIndex = ' , h)
+    let playerSteps;
+    if(playersign === 'X'){
+        playerSteps = xPlayer;
+    }else{
+        playerSteps = yPlayer;
     }
-    let v = rowIndex;
-    for(v ; v < rowIndex + resultNumber ; v++ ){
-        console.log('forRowIndex = ' , v)        
-    }
-*/
+    
+    // let winner;
+    let trueCounter = 0;
+
+    ruleStore3(colIndex, rowIndex)
+    .forEach((array, index) => {
+        trueCounter = 0;
+        array.forEach((val)=>{
+            if(playerSteps.includes(val)){
+                trueCounter += 1;
+                console.log('trueCounter = ', trueCounter);
+            };    
+            if(trueCounter == resultNumber -1){
+                console.log(index, `${playersign} NYERT !!!`);
+            }        
+        })
+    });
+}      
+
+const ruleStore3 = (colIndex, rowIndex)=>{
+
+    let rowControlNegative = [`${rowIndex}-${colIndex -1}` ,`${rowIndex}-${colIndex -2}`];
+    let rowControlPositive = [`${rowIndex}-${colIndex +1}` , `${rowIndex}-${colIndex +2}`];
+          
+    let colControlNegative = [`${rowIndex -1}-${colIndex}`, `${rowIndex -2}-${colIndex}`];
+    let colControlPositive = [`${rowIndex +1}-${colIndex}`, `${rowIndex +2}-${colIndex}`];
+            
+    let obliqueControlNegative1 = [`${rowIndex -1}-${colIndex -1}`, `${rowIndex -2}-${colIndex -2}`];
+    let obliqueControlNegative2 = [`${rowIndex -1}-${colIndex +1}` , `${rowIndex -2}-${colIndex +2}`];
+            
+    let obliqueControlPositive1 = [`${rowIndex +1}-${colIndex +1}`, `${rowIndex +2}-${colIndex +2}`];
+    let obliqueControlPositive2 = [`${rowIndex +1}-${colIndex -1}`, `${rowIndex +2}-${colIndex -2}`];
+    let resultNumber3 = [rowControlNegative, rowControlPositive, colControlNegative, colControlPositive, obliqueControlNegative1, obliqueControlNegative2, obliqueControlPositive1, obliqueControlPositive2];
+    
+    // console.log('ruleStore3 = ', resultNumber3)
+    return resultNumber3
+}
+  
 /* 
     kiszámolható, hogy az aktuális cellába kattintva nyert-e az illető a többi indexhez képest?
     max 10 cella/sor és 10 cella/oszlop
@@ -64,45 +78,48 @@ const winnerTest = (id)=>{
 
     majd a meglévő számokat megkeresni a xPlayer vagy az yPlayer tömbökben ]
 */
-}
+
 
 const buildGameGround = ()=>{
 
     const playGround = document.querySelector('#playGround');
-    let playerIcon = 'X';
-     
+    
     for(let v = 0; v < verticalCells; v += 1){
         const tr = elFactory('tr', {id: `tr_${v}`, class: 'rows'}, );
         
         for(let h = 0; h < horizontalCells; h += 1){
             const td = elFactory('td', {id:`${v}-${h}`, class:'cells free'}, ' ');
             
-                td.addEventListener('click', (e)=>{
-                    console.log('id = ', e.target.getAttribute('id'));
-                    if(e.target.getAttribute('class').indexOf('free') >-1){
-
-                        e.target.innerHTML = playerIcon;  
-                        e.target.setAttribute('class', 'cells busy');           
-                        winnerTest(e.target.getAttribute('id'));
-                        if(playerIcon == 'X'){
-                            e.target.style.background = 'green';
-                            xPlayer.push(e.target.getAttribute('id'));
-                            playerIcon = '0';
-                        }else{
-                            e.target.style.background = 'red';
-                            yPlayer.push(e.target.getAttribute('id'));
-                            playerIcon = 'X';
-                        }
-                        console.log('xPlayer = ', xPlayer);
-                        console.log('yPlayer = ', yPlayer);
-                    }else{                    
-                    }
-                });
+            td.addEventListener('click', addClickEvents);
             
             tr.appendChild(td);
         }
         playGround.appendChild(tr);
         
+    }
+}
+
+const addClickEvents = (e)=>{
+    // console.log('id = ', e.target.getAttribute('id'));
+    if(e.target.getAttribute('class').indexOf('free') >-1){
+
+        e.target.innerHTML = playerIcon;  
+        e.target.setAttribute('class', 'cells busy');           
+        // console.log(winnerTest(e.target.getAttribute('id')));
+        if(playerIcon == 'X'){
+            e.target.style.background = 'green';
+            winnerTest(e.target.getAttribute('id'), 'X');
+            xPlayer.push(e.target.getAttribute('id'));
+            playerIcon = '0';
+        }else{
+            e.target.style.background = 'red';
+            winnerTest(e.target.getAttribute('id'), '0');
+            yPlayer.push(e.target.getAttribute('id'));
+            playerIcon = 'X';
+        }
+        console.log('xPlayer = ', xPlayer);
+        console.log('yPlayer = ', yPlayer);
+    }else{                    
     }
 }
 
