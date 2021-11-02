e89414d620451748137792a53faa85a9f8941d5d
Zolival <zolinak@gmail.com>
2021-11-02 20:39:32 +0100

    overthought, but it works

--- js-1-feladatok-tictactoe/js.js
+++ js-1-feladatok-tictactoe/js.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const horizontalCells = 5;
-const verticalCells = 5;
-let resultNumber = 3 ; //or 5
+let horizontalCells;
+let verticalCells;
+let resultNumber = 3 ; //((or 5))
+let playRound = 0 ;
 let xPlayer = [];
 let yPlayer = [];
 
 let playerIcon = 'X';
 
-const winnerTest = (id, playersign)=>{
+const winnerTest = (id, playerSign) => {
     let indexOf = id.indexOf('-');
     let rowIndex = parseFloat(id.slice(0, indexOf));
     let colIndex = parseFloat(id.slice(indexOf+1 , ));
     let playerSteps;
-    if(playersign === 'X'){
+    if(playerSign === 'X'){
         playerSteps = xPlayer;
     }else{
         playerSteps = yPlayer;
@@ -31,14 +32,28 @@ const winnerTest = (id, playersign)=>{
                 console.log('trueCounter = ', trueCounter);
             };    
             if(trueCounter == resultNumber -1){
-                console.log(index, `${playersign} NYERT !!!`);
+                console.log(index, `${playerSign} NYERT!`);
+                massage('.massage', `${playerSign} NYERT !!!`)
+                removeAllEvents();
             }        
         })
     });
-}      
+}
+const massage = (destination, massage) => {
+    document.querySelector(destination).innerHTML = massage;
+}
 
-const ruleStore3 = (colIndex, rowIndex)=>{
+const removeAllEvents = () => {
+    document.querySelectorAll('.free')
+        .forEach(v=>{v.setAttribute('class', 'cells blank')}) ;
 
+    document.querySelectorAll('.cells')
+        .forEach(e => e.removeEventListener('click', addClickEvents));
+
+}
+
+const ruleStore3 = (colIndex, rowIndex) => {
+// Erre ki kell találni jobb megoldást! Van ötletetek?
     let rowControlNegative = [`${rowIndex}-${colIndex -1}` ,`${rowIndex}-${colIndex -2}`];
     let rowControlPositive = [`${rowIndex}-${colIndex +1}` , `${rowIndex}-${colIndex +2}`];
     let centerControl1 = [`${rowIndex}-${colIndex -1}` ,`${rowIndex}-${colIndex +1}`];
@@ -47,46 +62,51 @@ const ruleStore3 = (colIndex, rowIndex)=>{
     let colControlPositive = [`${rowIndex +1}-${colIndex}`, `${rowIndex +2}-${colIndex}`];
     let centerControl2 = [`${rowIndex -1}-${colIndex}`, `${rowIndex +1}-${colIndex}`];
             
-    let obliqueControlNegative1 = [`${rowIndex -1}-${colIndex -1}`, `${rowIndex -2}-${colIndex -2}`];
-    let obliqueControlNegative2 = [`${rowIndex -1}-${colIndex +1}` , `${rowIndex -2}-${colIndex +2}`];
+    let diagonalControlNegative1 = [`${rowIndex -1}-${colIndex -1}`, `${rowIndex -2}-${colIndex -2}`];
+    let diagonalControlNegative2 = [`${rowIndex -1}-${colIndex +1}` , `${rowIndex -2}-${colIndex +2}`];
     let centerControl3 = [`${rowIndex +1}-${colIndex +1}` , `${rowIndex -1}-${colIndex -1}`];
             
-    let obliqueControlPositive1 = [`${rowIndex +1}-${colIndex +1}`, `${rowIndex +2}-${colIndex +2}`];
-    let obliqueControlPositive2 = [`${rowIndex +1}-${colIndex -1}`, `${rowIndex +2}-${colIndex -2}`];
+    let diagonalControlPositive1 = [`${rowIndex +1}-${colIndex +1}`, `${rowIndex +2}-${colIndex +2}`];
+    let diagonalControlPositive2 = [`${rowIndex +1}-${colIndex -1}`, `${rowIndex +2}-${colIndex -2}`];
     let centerControl4 = [`${rowIndex +1}-${colIndex -1}`, `${rowIndex -1}-${colIndex +1}`]; 
 
-    let resultNumber3 = [rowControlNegative, rowControlPositive, colControlNegative, colControlPositive, obliqueControlNegative1, obliqueControlNegative2, obliqueControlPositive1, obliqueControlPositive2, centerControl1, centerControl2, centerControl3, centerControl4];
+    let resultNumber3 = [rowControlNegative, rowControlPositive, colControlNegative, colControlPositive, diagonalControlNegative1, diagonalControlNegative2, diagonalControlPositive1, diagonalControlPositive2, centerControl1, centerControl2, centerControl3, centerControl4];
     // console.log('ruleStore3 = ', resultNumber3)
     return resultNumber3
 }
-  
-/* 
-    kiszámolható, hogy az aktuális cellába kattintva nyert-e az illető a többi indexhez képest?
-    max 10 cella/sor és 10 cella/oszlop
-    idnumber
-    vízszintesen
-    (ha kisebb mint max rowindex -3)
-    +1, +2 (+3, +4)
-    (ha nagyobb, mint X3)
-    -1, -2 (-3, -4)
+
+const restartGame = () => {
+    document.querySelector('#playGround__'+ playRound)
+        .setAttribute('class', 'table hidden');
+        massage('.massage', '')
+        playRound += 1;
+        document.querySelector('#container')
+        .appendChild(elFactory('table', {class: 'table', id: 'playGround__'+ playRound}));
+    verticalCells = document.querySelector('#rowNumber').value;
+    horizontalCells = document.querySelector('#colNumber').value;
+    xPlayer = [];
+    yPlayer = [];
+    playerIcon = 'X'
+    buildGameGround();
+}
+
+const startGame = () => {
+    playRound +=1
+    document.querySelector('#container')
+        .appendChild(elFactory('table', {class: 'table', id: 'playGround__'+ playRound}));
+        
+    verticalCells = document.querySelector('#rowNumber').value;
+    horizontalCells = document.querySelector('#colNumber').value;
+
+    let starts__btn = document.querySelector('#starts__btn');
+    starts__btn.innerHTML = 'Restart game';
+    starts__btn.onclick = restartGame;
     
-    függőlegesen
-    + 10, +20 (+30, +40)
-    (ha nagyobb, mint X3)
-    -10, -20
-
-    sréhen
-    +11, +12 (+13, +14)
-    (ha nagyobb, mint 29)
-    -11, -12 (-13, -14)
-
-    majd a meglévő számokat megkeresni a xPlayer vagy az yPlayer tömbökben ]
-*/
-
+    buildGameGround();
+}
 
 const buildGameGround = ()=>{
-
-    const playGround = document.querySelector('#playGround');
+    const playGround = document.querySelector('#playGround__'+ playRound);
     
     for(let v = 0; v < verticalCells; v += 1){
         const tr = elFactory('tr', {id: `tr_${v}`, class: 'rows'}, );
@@ -100,16 +120,14 @@ const buildGameGround = ()=>{
         }
         playGround.appendChild(tr);
         
-    }
+    } 
 }
 
 const addClickEvents = (e)=>{
-    // console.log('id = ', e.target.getAttribute('id'));
     if(e.target.getAttribute('class').indexOf('free') >-1){
 
         e.target.innerHTML = playerIcon;  
         e.target.setAttribute('class', 'cells busy');           
-        // console.log(winnerTest(e.target.getAttribute('id')));
         if(playerIcon == 'X'){
             e.target.style.background = 'green';
             winnerTest(e.target.getAttribute('id'), 'X');
@@ -121,14 +139,12 @@ const addClickEvents = (e)=>{
             yPlayer.push(e.target.getAttribute('id'));
             playerIcon = 'X';
         }
-        console.log('xPlayer = ', xPlayer);
-        console.log('yPlayer = ', yPlayer);
     }else{                    
     }
 }
 
 
-const elFactory = (type, attributes, content)=> {
+const elFactory = (type, attributes, content) => {
     if(type){
         const el = document.createElement(type);
         if(attributes){
@@ -143,5 +159,3 @@ const elFactory = (type, attributes, content)=> {
         return el
     }
 }
-
-buildGameGround();
