8a1d2274ae1842549c423210a1ab2b71400e47db
Zolival <zolinak@gmail.com>
2021-12-04 14:34:36 +0100

    I'm working...

--- js-4-feladatok-game-of-thrones/js/got.js
+++ js-4-feladatok-game-of-thrones/js/got.js
@@ -1,14 +1,33 @@
 // 'use strict';
 
-const heroes = async (url = './json/got.json') => {
+const getHeroes = async (url = './json/got.json') => {
     try {
         const response = await fetch(url);
         const dataArr = await response.json();
-        const shortedData = sortedByHeroesName(dataArr)
-        return shortedData
+        const livingHeros = livingFilter(dataArr)
+        const shortedData = sortedByHeroesName(livingHeros)
+        elementFactorys(shortedData);
+        addEvents();
+        // searchEvents(shortedData);
+        return livingHeros
     } catch (err) {
-        error.log(`Error! Massage: ${err} ; sourse: ${url}`)
+        console.error(`Error! Massage: ${err} ; sourse: ${url}`)
     }
+
+}
+const searchHeroes = (arr, name) => {
+    const image = document.querySelector('.heroes__Img');
+    const heroesName = document.querySelector('.heroes-name');
+    const hauseArms = document.querySelector('.house_coat_of_arms__img');
+    const description = document.querySelector('.short_description');
+    arr.forEach((item) => {
+        if (item.hasOwnProperty(name) == name) {
+            image.setAttribute('src', `./${item.portrait}`);
+            heroesName.textContent = item.name;
+            hauseArms.setAttribute('src', `./${item.portrait}`);
+            description.textContent = item.bio;
+        }
+    })
 }
 
 const sortedByHeroesName = (arr = [{}]) => {
@@ -16,16 +35,68 @@ const sortedByHeroesName = (arr = [{}]) => {
         const aName = a.name.toLowerCase();
         const bName = b.name.toLowerCase();
 
-    if (aName > bName) {
-        return 1;
-    } else if (aName < bName) {
-        return -1;
-    } else {
-        return 0;
-    }
-});
+        if (aName > bName) {
+            return 1;
+        } else if (aName < bName) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
     return arr
 }
 
-console.log('heroes() = ', heroes());
+const livingFilter = (arr = [{}]) => (
+    arr.filter(item => item.hasOwnProperty('name') && item.hasOwnProperty('picture') && !item.hasOwnProperty('dead'))
+)
+
+const elementFactorys = (arr = [{}]) => {
+    const main = document.querySelector('.main-container');
+
+    arr.forEach(item => {
+        const img = elFactory('img', { src: `.\/${item.portrait}`, class: 'images' });
+        const p = elFactory('p', { class: 'heroes_names' }, item.name);
+        const div = elFactory('div', { class: 'divs' }, img, p);
+        main.appendChild(div);
+    });
+}
+
+const elFactory = (type, attributes, ...children) => {
+    const el = document.createElement(type);
+
+    for (let key in attributes) {
+        el.setAttribute(key, attributes[key]);
+    }
+
+    children.forEach((child) => {
+        if (typeof child === "string") {
+            el.appendChild(document.createTextNode(child));
+        } else {
+            el.appendChild(child);
+        }
+    });
+    return el;
+}
+
+// kialakítás alatt:
+const addClickEvents = (e) => {
+
+    console.log('event activate', e.target);
+    console.log(e.target.getAttribute('src'));
+    console.log('event activate', e.target.getAttribute('heroes_names'));
+}
+
+const addEvents = () => {
+    document.querySelectorAll('.heroes_names')
+        .forEach((item) => item.addEventListener(`click`, addClickEvents));
+};
+
+
+/*
+const searchEvents = () => {
+    
+}
+const clickEvent = () => (item, div)
+*/
+console.log('heroes() = ', getHeroes());
 console.log('code end');
